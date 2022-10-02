5e4cc6379be38f45af6827b56c3cc158e59e160a
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-02 13:15:21 +0530

    Create Ballot.sol
    init contract scaffolding

---
+++ contracts/Ballot.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/// @title Voting with delegation.
+/** @notice Purpose of contract is to  
+- give voting rights, 
+- casting votes, 
+- delegating votes and 
+- querying results
+*/
+/** @dev //TO-DO
+ */
+contract Ballot {
+    address public chairperson;
+}
